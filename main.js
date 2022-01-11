fc7975c12da6422a55cadca923b4ecb043d138b2
YOUR_USERNAME <latania.reece@outlook.com>
2022-01-11 16:35:40 -0500

    updates

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-document.getElementById("portfolio-scroll").addEventListener("click", ()=>{
-    document.getElementById("portfolio").scrollIntoView({
-        behavior: 'smooth' 
-    });
-})
+document.getElementById('portfolio-scroll').addEventListener('click', () => {
+  document.getElementById('portfolio').scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+// document.getElementById('portfolio-link').addEventListener('click', () => {
+//   // direct browser to top right away
+//   if (window.location.hash) scroll(0, 0);
+//   // takes care of some browsers issue
+//   setTimeout(function () {
+//     scroll(0, 0);
+//   }, 1);
+// });
