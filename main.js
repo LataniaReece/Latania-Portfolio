bdf6f5a462a4eef46e4bbca74578eb9b14f4dd01
YOUR_USERNAME <latania.reece@outlook.com>
2022-01-15 22:39:58 -0500

    updates

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ document.getElementById('portfolio-scroll').addEventListener('click', () => {
   });
 });
 
-// document.getElementById('portfolio-link').addEventListener('click', () => {
-//   // direct browser to top right away
-//   if (window.location.hash) scroll(0, 0);
-//   // takes care of some browsers issue
-//   setTimeout(function () {
-//     scroll(0, 0);
-//   }, 1);
-// });
+document.querySelector('.contact-btn').addEventListener('click', () => {
+  document.getElementById('contact').scrollIntoView();
+});
