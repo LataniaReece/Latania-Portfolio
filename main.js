ef33e306d48c912c278b35ee9ad9cf6f09d319d6
YOUR_USERNAME <latania.reece@outlook.com>
2022-01-23 08:49:41 -0500

    some animations removed

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ window.onbeforeunload = function () {
 const loader = document.getElementById('preloader');
 window.addEventListener('load', () => {
   loader.style.display = 'none';
+  document.getElementById('overlay').style.opacity = '0';
 });
 
 // GSAP Animation
@@ -32,7 +33,6 @@ window.addEventListener('scroll', () => {
   const windowPosition = window.scrollY < 40;
 
   if (!windowPosition) {
-    console.log('hello');
     shapesEl.classList.add('scrolling-fade-out');
     shapesEl.classList.remove('scrolling-fade-in');
   } else {
@@ -44,23 +44,36 @@ window.addEventListener('scroll', () => {
 const hamburgerEl = document.querySelector('.hamburger');
 const navListContainerEl = document.querySelector('.nav-list-container');
 const headerContentEl = document.querySelector('header .content');
+const closeEl = document.querySelector('.close');
+const navLinks = document.querySelectorAll('.nav-list li');
 
 hamburgerEl.addEventListener('click', () => {
-  if (navListContainerEl.classList.contains('open')) {
-    // Close the menu
-    navListContainerEl.classList.remove('open');
-    headerContentEl.style.zIndex = '1';
-    hamburgerEl.classList.remove('faded');
-    navListContainerEl.classList.remove('mobile-menu');
-    navListContainerEl.classList.remove('fade-in');
-  } else {
-    // Open the menu
-    navListContainerEl.classList.add('open');
-    headerContentEl.style.zIndex = '-1';
-    hamburgerEl.classList.add('faded');
-    navListContainerEl.classList.add('mobile-menu');
-    navListContainerEl.classList.add('fade-in');
-  }
+  document.getElementById('overlay').style.opacity = '1';
+  navListContainerEl.classList.add('open');
+  headerContentEl.style.zIndex = '-1';
+  hamburgerEl.classList.add('faded');
+  navListContainerEl.classList.add('mobile-menu');
+  navListContainerEl.classList.add('fade-in');
+});
+
+const closeHeaderAndRemoveOverlay = () => {
+  navListContainerEl.classList.remove('open');
+  document.getElementById('overlay').style.opacity = '0';
+  navListContainerEl.classList.remove('open');
+  headerContentEl.style.zIndex = '0';
+  hamburgerEl.classList.remove('faded');
+  navListContainerEl.classList.remove('mobile-menu');
+  navListContainerEl.classList.remove('fade-in');
+};
+
+closeEl.addEventListener('click', () => {
+  closeHeaderAndRemoveOverlay();
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    closeHeaderAndRemoveOverlay();
+  });
 });
 
 document.querySelectorAll('.projects-link').forEach((link) => {
